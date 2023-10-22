4f86d5d3278ae48fa00a664db4cac5117d4eb5b4
khoanvdse173299 <khoanvdse173299@fpt.edu.vn>
2023-10-23 00:32:11 +0700

    Update cors

--- src/main/java/com/example/birdReproductionManagement/CorsConfig.java
+++ src/main/java/com/example/birdReproductionManagement/CorsConfig.java
@@ -6,18 +6,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer getCorsConfiguration() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
